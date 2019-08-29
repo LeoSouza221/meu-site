3199d57ded29cffd7bf1a609634a57d7748fe53c
Leo Souza <leosouza221@gmail.com>
2019-08-29 17:44:53 -0300

    Inicio adicao de funcao para botoes a direita

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,7 +1,3 @@
-let button_left_1;
-let button_left_2;
-let button_left_3;
-let button_left_4;
 let select = 0;
 let select_item = document.getElementById("item-select");
 let i = 0;
@@ -10,12 +6,15 @@ function adjustSelectItem(current, next) {
   select_item.children[current].classList.remove('selected-menu-item');
   select = next;
   select_item.children[select].classList.add('selected-menu-item');
+  // console.log(select_item.children[select].firstChild.href)
+  const right = document.getElementsByClassName('right-button');
+  right[0].getElementsByTagName("a").href = select_item.children[select].firstChild.href;
+  // console.log(right[0].getElementsByTagName("a").href);
 }
 
 const button_action = {
   DOWN: () => {
     if (select === 3) {
-      console.log('aki');
       adjustSelectItem(select, 0);
       return;
     }
@@ -34,23 +33,30 @@ function markSelectedItem(event, button_press) {
   let i = 0;
   if (!event && button_press) {
     button_action[button_press]();
-
     return;
   } else if (!event && !button_press) {
     select_item.children[select].classList.add('selected-menu-item');
     return;
   }
-  // if (event.target.nodeName = select_item.children[select].nodeName) {
   for (i; i < select_item.children.length; i++) {
     if (event.target == select_item.children[i]) {
       adjustSelectItem(select, i);
     }
   }
-  // }
 }
 
 select_item.addEventListener("mouseover", function(event) {
   markSelectedItem(event);
+});
+
+let buttons_left = document.addEventListener("keydown", event => {
+  if (event.keyCode === 40) {
+    button_action.DOWN();
+    return;
+  }
+  if (event.keyCode === 38) {
+    button_action.UP();
+  }
 });
 
 window.onload = markSelectedItem();
