be1a6829fb9b14128ce9dd92171cb5080f100b06
Leo Souza <leosouza221@gmail.com>
2019-09-20 17:56:00 -0300

    Adicionado versao inicial mobile

--- resources/javascript/user_screen.js
+++ resources/javascript/user_screen.js
@@ -1,3 +1,15 @@
 const skills_count = document.getElementsByClassName('knowledge-bookcase');
 const show_skills = document.querySelector('.skills-amount');
 show_skills.innerHTML = skills_count[0].children.length - 3;
+
+const menu_opening = document.querySelector('.menu');
+
+function openMenu() {
+  console.log('aki')
+  menu_opening.style.animation = "menu_slide 2s forwards";
+}
+
+function closeMenu() {
+  console.log('aki')
+  menu_opening.style.animation = "menu_slide 2s inverse forwards";
+}
