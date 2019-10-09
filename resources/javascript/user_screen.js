ded32dafe0802dc2d61648d6c267d96dd63e093f
Leo Souza <leosouza221@gmail.com>
2019-10-09 09:42:55 -0300

    Requisicao api github e alterações gerais

--- resources/javascript/user_screen.js
+++ resources/javascript/user_screen.js
@@ -26,16 +26,56 @@ function closeMenu() {
   menu_opening.style.animation = "menu_slide 2s reverse forwards";
 }
 
-function defineImage(event) {
-  console.log(event)
+function getRepositories() {
+  fetch('https://api.github.com/users/LeoSouza221/repos', { method: 'get' })
+    .then((response) => {
+      response.json()
+        .then((result) => { 
+          console.log(result); 
+        }) 
+    })
+    .catch(error => console.log(error));
 }
 
-function randomImage() {
-  
-}
+// function defineImage(event) {
+//   console.log(event)
+// }
 
-window.onload = randomImage();
+// function alterElementAttributes(element, item) {
+//   element.getElementsByTagName('a').href = item.link;
+//   element.getElementsByTagName('img').src = item.src;
+//   return;
+// }
 
-alter_image.addEventListener("click", function(event) {
-  defineImage(event);
-});
+// function createElementAttributes(element, item) {
+//   const a = document.createElement('a');
+//   const img = document.createElement('img');
+//   let create_link = element.appendChild(a);
+//   create_link.href = item.link;
+//   let create_img = element.appendChild(img)
+//   create_img.src = item.src;
+//   create_img.width = 30;
+//   create_img.height = 30;
+//   return;
+// }
+
+// function randomImage() {
+//   setInterval(() => {
+//     images_array.forEach((item) => {
+//       const element = alter_image.getElementsByClassName('contact-box-back')[0];
+//       const element_child = element.children;
+//       if (element_child.length > 0) {
+//         alterElementAttributes(element, item);
+//         return;
+//       }
+//       createElementAttributes(element, item);
+//       console.log('aki')
+//     });
+//   }, 500)
+// }
+
+window.onload = getRepositories();
+
+// alter_image.addEventListener("click", function(event) {
+//   defineImage(event);
+// });
